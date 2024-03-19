2445cfea0c09072ee960e554f02a4d72734922f7
Kmeadows3 <kmeadows3@gmail.com>
2024-03-19 07:59:43 -0400

    got program to launch, renamed main application

--- src/main/java/my/TNTBuilder/TNTBuilderApplication.java
+++ src/main/java/my/TNTBuilder/TNTBuilderApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TenmoApplication {
+public class TNTBuilderApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TenmoApplication.class, args);
+        SpringApplication.run(TNTBuilderApplication.class, args);
     }
 
 }

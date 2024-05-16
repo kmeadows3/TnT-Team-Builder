bfcb659b8d805ab316a08841c9a4c4c786665728
Kmeadows3 <kmeadows3@gmail.com>
2024-05-16 16:51:07 -0400

    Added method in controller to purchase item

--- TNT-Server/src/main/java/my/TNTBuilder/controller/UnitController.java
+++ TNT-Server/src/main/java/my/TNTBuilder/controller/UnitController.java
@@ -5,6 +5,7 @@ import my.TNTBuilder.exception.DaoException;
 import my.TNTBuilder.exception.ServiceException;
 import my.TNTBuilder.model.Skill;
 import my.TNTBuilder.model.Unit;
+import my.TNTBuilder.model.inventory.Item;
 import my.TNTBuilder.service.UnitService;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -135,6 +136,26 @@ public class UnitController {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
         }
     }
+
+
+    /**
+     * Adds a new item to the unit inventory
+     * @param itemId the skill to be added
+     * @param unitId the id of the unit
+     * @param principal the logged-in user
+     */
+    @ResponseStatus(HttpStatus.CREATED)
+    @RequestMapping(path="/units/{unitId}/inventory", method = RequestMethod.POST)
+    public void addSkillToUnit(@RequestBody Integer itemId, @PathVariable int unitId, Principal principal){
+        try {
+            unitService.purchaseItemForUnit(itemId, unitId, userDao.getUserIdByUsername(principal.getName()));
+        } catch (ServiceException e){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        } catch (DaoException e){
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+        }
+    }
+
 
     /**
      * Returns a specific unit based on the unit's id
